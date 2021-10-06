717b6e923843b524adde9a741e46dc37de06c4aa
dmitriypur <vst-pcmusic@yandex.ru>
2021-10-06 14:00:44 +0300

    validate

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductStoreRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -25,9 +26,10 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductStoreRequest $request)
     {
-        //
+        $created_product = Product::create($request->validated());
+        return new ProductResource($created_product);
     }
 
     /**
